ed0f1f1b6a9b5d5dd61677e537a02844a784535d
khalidyatim19 <2020489056@student.uitm.edu.my>
2022-05-04 10:53:57 +0800

    third commit

--- handle_form.php
+++ handle_form.php
@@ -7,13 +7,44 @@
     <title>Form Feedback</title>
 </head>
 <body>
-<?php #script handle form 
+if (strlen($_POST["name"]) >0){
+    $_POST["name"] = $_POST["name"];
+}
+else{
+    $_POST["name"]= null;
 
-echo "<p> Thank you, <b>" .$_POST["name"]. 
-"</b> for the following comments: <br/>". 
-$_POST["comments"]."<p>We will reply to you at <i>". 
-$_POST["email"]."</i><>/p>";
+    echo '<p><b> You Forgot to Enter Your Name! </b></p>';  
+}
+if (strlen($_POST["comments"]) >0){
+    $_POST["comments"] = $_POST["comments"];
+}
+else{
+    $_POST["comments]"]= null;
 
-?>
+    echo '<p><b> You Forgot to Enter Your Comments! </b></p>';  
+}
+if(!(strlen($_POST["email"]) >0)){
+    $_POST["email"] = null;
+    echo '<p><b> You Forgot to Enter Your Emails! </b></p>';
+}
+
+if(isset($_POST["gender"])){
+    if($_POST["gender"] == 'M'){
+        $message = '<b><p>Good Day, Sir </b></p>';
+    }
+    if ($_POST["gender"] == 'F'){
+        $message = '<b><p>Good Day, Madam </b></p>';)
+}else{
+    $_POST["gender"] = null;
+
+    echo '<p><b> You forgot to choose your gender! </b></p>;
+}
+
+if ($_POST["name"] && $_POST["comments"] && $_POST["email"] && $_POST["gender"]) {
+    echo "Thank You," .$_POST["name"]. "for the following comment: <br/><tt>" . 
+    $_POST["comment"]. "</tt> <p> we will reply to you at <i>" . $_POST["email"].
+    "</i></p>";
+    echo $message;
+}
 </body>
 </html>
